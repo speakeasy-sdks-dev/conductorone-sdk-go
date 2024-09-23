a556143d8e577372861ae1e2cbd642397fd3b60b
regression-test-executor <regression-test-executor@speakeasy.com>
2024-09-23 15:13:52 +0000

    openapi-generation@6849e2056a23c26ad0eafba426dfaada77de04d3

--- conductoroneapi.go
+++ conductoroneapi.go
@@ -42,6 +42,9 @@ func Float32(f float32) *float32 { return &f }
 // Float64 provides a helper function to return a pointer to a float64
 func Float64(f float64) *float64 { return &f }
 
+// Pointer provides a helper function to return a pointer to a type
+func Pointer[T any](v T) *T { return &v }
+
 type sdkConfiguration struct {
 	Client            HTTPClient
 	Security          func(context.Context) (interface{}, error)
@@ -199,8 +202,8 @@ func New(opts ...SDKOption) *ConductoroneAPI {
 			Language:          "go",
 			OpenAPIDocVersion: "0.1.0-alpha",
 			SDKVersion:        "1.19.0",
-			GenVersion:        "2.386.2",
-			UserAgent:         "speakeasy-sdk/go 1.19.0 2.386.2 0.1.0-alpha github.com/conductorone/conductorone-sdk-go",
+			GenVersion:        "2.422.3",
+			UserAgent:         "speakeasy-sdk/go 1.19.0 2.422.3 0.1.0-alpha github.com/conductorone/conductorone-sdk-go",
 			ServerDefaults: []map[string]string{
 				{
 					"tenantDomain": "example",
